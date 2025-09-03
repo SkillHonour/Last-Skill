d30d3c6b1f8cfc94733e9f3c2828da5d0ff6fd83
SkillHonour <Skillhonour@gmail.com>
2025-09-03 11:27:07 +0530

    nav

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,43 +1,52 @@
 import React, { useState } from "react";
-import './Navbar.css';
+import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function Navbar() {
-    // State to toggle mobile menu
-    const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-    // Toggle mobile menu on button click
-    const toggleMenu = () => {
-        setIsMobile(!isMobile);
-    };
+  const toggleMenu = () => {
+    setIsMobile(!isMobile);
+  };
 
-    // Close the menu after clicking a link
-    const closeMenu = () => {
-        setIsMobile(false);
-    };
+  const closeMenu = () => {
+    setIsMobile(false);
+  };
 
-    return (
-        <div className="overallnav">
-            <div className="brandname">
-                <h1>Skill Honor</h1>
-            </div>
+  return (
+    <nav className="overallnav">
+      {/* Brand Name */}
+      <div className="brandname">
+        <h1>Skill Honor</h1>
+      </div>
 
-            {/* Hamburger Menu Icon for Mobile */}
-            <div className="hamburger" onClick={toggleMenu}>
-                <div className="line"></div>
-                <div className="line"></div>
-                <div className="line"></div>
-            </div>
+      {/* Menu Links */}
+      <ul className={`navbarlink ${isMobile ? "active" : ""}`}>
+        <li>
+          <Link className="navlink" to="/" onClick={closeMenu}>Home</Link>
+        </li>
+        <li>
+          <Link className="navlink" to="/service" onClick={closeMenu}>Service</Link>
+        </li>
+        <li>
+          <Link className="navlink" to="/portfolio" onClick={closeMenu}>Portfolio</Link>
+        </li>
+        <li>
+          <Link className="navlink" to="/about" onClick={closeMenu}>About</Link>
+        </li>
+        <li>
+          <Link className="navlink" to="/content" onClick={closeMenu}>Contact</Link>
+        </li>
+        <li>
+          <Link className="navlink" to="/testimonial" onClick={closeMenu}>Testimonial</Link>
+        </li>
+      </ul>
 
-            {/* Navbar Links */}
-            <div className={`navbarlink ${isMobile ? 'active' : ''}`}>
-                <Link className="navlink" to={'/'} onClick={closeMenu}>Home</Link>
-                <Link className="navlink" to={"/service"} onClick={closeMenu}>Service</Link>
-                <Link className="navlink" to={"/portfolio"} onClick={closeMenu}>Portfolio</Link>
-                <Link className="navlink" to={'/about'} onClick={closeMenu}>About</Link>
-                <Link className="navlink" to={'/content'} onClick={closeMenu}>Contact</Link>
-                <Link className="navlink" to={'/testimonial'} onClick={closeMenu}>Testimonial</Link>
-            </div>
-        </div>
-    );
+      {/* Mobile Menu Icon */}
+      <div className="hamburger" onClick={toggleMenu}>
+        {isMobile ? <FaTimes /> : <FaBars />}
+      </div>
+    </nav>
+  );
 }
